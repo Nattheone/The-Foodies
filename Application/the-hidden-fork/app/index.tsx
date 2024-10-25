c685def84a674feb9ac46bf1bd64f6acda522287
Nathalie Zamarron <135666386+Nattheone@users.noreply.github.com>
2024-10-25 15:13:27 +0000

    assets added

--- Application/the-hidden-fork/app/index.tsx
+++ Application/the-hidden-fork/app/index.tsx
@@ -1,16 +1,56 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
+import { useRouter } from 'expo-router';
+import backgroundImage from '../assets/unsplash_B-DrrO3tSbo.png';
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ImageBackground
+      source={require('./app/assets/unsplash_B-DrrO3tSbo.png')} 
+      style={styles.background}
     >
-      <Text>deuce is my dog</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>DISCOVER & ENJOY</Text>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title="SIGN UP"
+            color="#6A7E61" // Customize the button color
+            onPress={() => router.push('./auth/SignUp')} // Navigate to Sign Up screen
+          />
+          <Button
+            title="LOGIN"
+            color="#6A7E61" // Customize the button color
+            onPress={() => router.push('./auth/Login')} // Navigate to Login screen
+          />
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    color: 'white',
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    width: '80%',
+    justifyContent: 'space-between',
+    height: 120,
+  },
+});
